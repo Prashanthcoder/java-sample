89eee4127f15ae7a6a2b09dc36230e109ec3e79a
Prashanthcoder <tp120113@gmail.com>
2026-02-16 22:57:45 +0530

    Added the code features of atm machine to work within conditions

--- Conditions1.java
+++ Conditions1.java
@@ -4,24 +4,43 @@ class Conditions1{
 public static void main(String[] args)
 {
 Scanner sc = new Scanner(System.in);
-int amount;
+Conditions1 s = new Conditions1();
 int balance = 500;
-String response = "yes";
-while(!response.equals("exit")){
-System.out.println(" \n\n\n\n\n\n----------------------------Welcome to the Prash ATM!------------------------------------ \n\n\n\nDo you want to continue? type (yes/exit)	\n");
-response = sc.nextLine();
-System.out.println("Enter the amount to withdraw");
-amount = sc.nextInt();
-if(amount<=500){
-System.out.println("Amount withdrawed !");
-balance = balance - amount;
-System.out.println("Available balance after withdraw: "+balance);
+System.out.println("-----------------------WELCOME----TO--------SHOBHA---------BANK----------------");
+System.out.println("Enter the choice of service: \n1.Withdraw \n2.Deposit \n3.Exit ");
+int choice = sc.nextInt();
+if(choice == 1){
+	System.out.println("Enter the amount you want to withdraw ");
+	int amount = sc.nextInt();
+	balance = s.withdraw(amount, balance);
+}else if(choice == 2){
+	System.out.println("Enter the amount you want to deposit ");
+	int amount = sc.nextInt();
+	balance = s.deposit(amount, balance);
 }
 else{
-System.out.println("Insuffiecient balance!");
-System.out.println("Available balance: "+balance);
+	System.out.println("Please enter between 3 choices only ");
 }
+
 }
+
+int withdraw(int amount, int balance){
+	if(balance<amount)
+	{
+	System.out.println("Insufficient balance! Check your balance: "+balance);
+	}
+	else{
+		balance = balance- amount;
+		System.out.println("Amount withdrawed! \n Current balance: "+balance);
+	}
+	return balance;
+}
+
+
+int deposit(int amount, int balance){
+	balance = balance + amount;
+	System.out.println("Amount deposited! \n current balance: "+balance);
+	return balance;
 }
 }
 			
