964b037faa2b22838f6cc7d9a975748659504692
Prashanthcoder <tp120113@gmail.com>
2026-02-18 23:55:26 +0530

    Added the two classes to find the areas of geometry of the functions

---
+++ task1/Areaof.java
@@ -0,0 +1,51 @@
+class Sample
+{
+static void AreaofSquare(int s){
+int area = s*s;
+System.out.println(area);	
+}
+static void AreaofRectangle(int l, int b){
+	int area = l*b;
+	System.out.println(area);
+}
+static void AreaofTriangle(int b, int h){
+	double area = 0.5*b*h;
+	System.out.println(area);
+}
+static void AreaofParallelogram(int b, int h){
+	int area = b*h;
+	System.out.println(area);
+}
+static void AreaofTrapezoid(int a, int b, int h){
+	double area = 0.5*(a+b)*h;
+System.out.println(area);
+}
+static void AreaofEllipse(int a, int b){
+	double pi = 3.1428;
+	double area = pi*a*b;
+	System.out.println(area);
+}
+static void AreaofCircle(double r){
+	double pi= 3.1428;
+	double area = pi*r*r;
+System.out.println(area);
+}
+static void AreaofSector(double r, double theta){
+	double area = 0.5*r*r*theta;
+System.out.println(area);
+}
+}
+
+class Areaof
+{
+public static void main(String[] args){
+	Sample.AreaofCircle(7.0);
+Sample.AreaofParallelogram(2, 3);
+Sample.AreaofSector(7.0, 3.1428);
+Sample.AreaofSquare(5);
+Sample.AreaofRectangle(4, 3);
+Sample.AreaofTriangle(7, 2);
+Sample.AreaofTrapezoid(1, 8, 3);
+Sample.AreaofEllipse(3, 7);
+}
+}
---
+++ task1/Sample.java
@@ -0,0 +1,66 @@
+class Sample
+{
+static void AreaofSquare(){
+int s = 5;
+int area = s*s;
+System.out.println(area);	
+}
+static void AreaofRectangle(){
+	int l = 3;
+	int b = 5;
+	int area = l*b;
+	System.out.println(area);
+}
+static void AreaofTriangle(){
+	int b = 5;
+	int h = 8;
+	double area = 0.5*b*h;
+	System.out.println(area);
+}
+static void AreaofParallelogram(){
+	int b =3;
+	int h=5;
+	int area = b*h;
+	System.out.println(area);
+}
+static void AreaofTrapezoid(){
+	int a = 8;
+	int b= 9;
+	int h = 6;
+	double area = 0.5*(a+b)*h;
+System.out.println(area);
+}
+static void AreaofEllipse(){
+	int a = 8;
+	int b = 3;
+	double pi = 3.1428;
+	double area = pi*a*b;
+	System.out.println(area);
+}
+static void AreaofCircle(){
+	double pi= 3.1428;
+	double r = 7.0;
+	double area = pi*r*r;
+System.out.println(area);
+}
+static void AreaofSector(){
+	double r = 7.0;
+	double theta = 3.142;
+	double area = 0.5*r*r*theta;
+System.out.println(area);
+}
+}
+
+class Demo
+{
+public static void main(String[] args){
+	Sample.AreaofCircle();
+Sample.AreaofPrallelogram();
+Sample.AreaofSector();
+Sample.AreaofSquare();
+Sample.AreaofRectangle();
+Sample.AreaofTriangle();
+Sample.AreaofTrapezoid();
+Sample.AreaofEllipse();
+}
+}
