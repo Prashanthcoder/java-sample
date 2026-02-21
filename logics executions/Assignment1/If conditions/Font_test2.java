a46afd11cb29fa8482ab152d4a45541a572eb5f1
Prashanthcoder <tp120113@gmail.com>
2026-02-21 22:48:16 +0530

    Added to check lower case in java

---
+++ logics executions/Assignment1/If conditions/Font_test2.java
@@ -0,0 +1,14 @@
+class Font_test2 
+{
+static void testing(char ch){
+	if(Character.isLowerCase(ch)){
+		System.out.println(ch+" is a lowercase letter");
+	}
+}
+	public static void main(String[] args) 
+	{
+		testing('i');
+testing('j');
+testing('w');
+	}
+}
