dac26aab9de3a8526c50f49876a3892b1bf448a4
Prashanthcoder <tp120113@gmail.com>
2026-02-21 22:29:17 +0530

    added the vote eligibility test

---
+++ logics executions/Assignment1/If conditions/VoteEligibilty.java
@@ -0,0 +1,13 @@
+class VoteEligibility 
+{
+static void agetest(int age){
+	if(age>=18){
+		System.out.println("This person is eligible to vote ");
+	}
+}
+	public static void main(String[] args) 
+	{
+		agetest(23);
+	agetest(18);
+	}
+}
