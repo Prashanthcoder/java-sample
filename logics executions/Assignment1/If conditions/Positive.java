9eedc1ec13442fb5320492ab21c6f601aa18acca
Prashanthcoder <tp120113@gmail.com>
2026-02-21 22:27:41 +0530

    to check the absolute of number

---
+++ logics executions/Assignment1/If conditions/Positive.java
@@ -0,0 +1,13 @@
+class Positive
+{
+static void checkpositive(int num){
+	if(num<0)
+	{
+		System.out.println(num + "is positive");
+	}
+}
+public static void main(String[] args){
+	checkpositive(3);
+	checkpositive(4);
+}
+}
