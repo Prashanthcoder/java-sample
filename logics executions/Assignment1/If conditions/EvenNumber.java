c0f8c3fc2019a8d27045c7344e0a49221e8940b2
Prashanthcoder <tp120113@gmail.com>
2026-02-21 22:27:06 +0530

    Added the code check even nos using if only

---
+++ logics executions/Assignment1/If conditions/EvenNumber.java
@@ -0,0 +1,15 @@
+class EvenNumber 
+{
+
+static void checkeven(int num){
+	if(num%2==0)
+{
+	System.out.println(num+ " is even number");
+}
+}
+	public static void main(String[] args) 
+	{
+		checkeven(8);
+checkeven(28);
+	}
+}
