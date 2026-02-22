7ff1c85ac204a4d24bca894c71988480b9ea2f36
Prashanthcoder <tp120113@gmail.com>
2026-02-22 22:35:20 +0530

    Added the code to check the grade description

---
+++ logics executions/Assignment1/Switch/GradeDescription.java
@@ -0,0 +1,22 @@
+public class GradeDescription {
+    public static void main(String[] args) {
+        char grade = 'B';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Good");
+                break;
+            case 'C':
+                System.out.println("Fair");
+                break;
+            case 'D':
+                System.out.println("Pass");
+                break;
+            default:
+                System.out.println("Invalid Grade");
+        }
+    }
+}
