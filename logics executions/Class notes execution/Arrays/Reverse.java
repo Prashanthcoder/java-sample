3022304467d0b71a4578a65d731f526a13aee1ab
Prashanthcoder <tp120113@gmail.com>
2026-02-27 00:00:44 +0530

    added the code using scanner class using string arrays to pass it to method and return and print it

--- logics executions/Class notes execution/Arrays/Reverse.java
+++ logics executions/Class notes execution/Arrays/Reverse.java
@@ -21,9 +21,18 @@ return res;
 public static void main(String[] args){
 	Reverse r1 = new Reverse();
 	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter the number of String you want to reverse ");
+	int size = sc.nextInt();
+	sc.nextLine();
+	String[] str = new String[size];
 	System.out.println("Enter the string to reverse");
-	String str = sc.nextLine();
-	System.out.println("\" "+r1.straight(str)+"\" "+ " this is not reversed cause the iteration is just traversal into string");
-	System.out.println("\" "+r1.reverse(str)+"\" "+" this is reversed cause it passed into the method reverse the reversal traversal and storage, concatenation");
-}
+	for(int i=0;i<size;i++){
+	System.out.print("String "+(i+1)+" : ");
+	str[i] = sc.nextLine();
+    }
+	for(int i = 0;i<size;i++){
+	System.out.println((i+1)+"______________________________________________________________________________________"+(i+1));
+	System.out.println("\""+r1.straight(str[i])+"\" "+ " this is not reversed cause the iteration is just traversal into string");
+	System.out.println("\""+r1.reverse(str[i])+"\" "+" this is reversed cause it passed into the method reverse the reversal traversal and storage, concatenation");
+}}
 }
