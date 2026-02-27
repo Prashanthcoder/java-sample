7c2a0051c8d4a18e77fa6aacae5795fe9676e2ec
Prashanthcoder <tp120113@gmail.com>
2026-02-27 21:51:20 +0530

    added the code for the array demo

---
+++ logics executions/Class notes execution/Sample.java
@@ -0,0 +1,13 @@
+public class Sample
+{
+public static void main(String[] args){
+	int[] arr = new int[5];
+	arr[0]= 2;
+    arr[1]= 34;
+    arr[3] = 30;
+
+for(int i=0;i<arr.length; i++){
+	System.out.println("arr["+i+"] = "+arr[i]);
+}
+}
+}
