51caf5d6907ce31a5dbff6d213d020256bf50331
Prashanthcoder <tp120113@gmail.com>
2026-03-01 23:27:43 +0530

    updated the code to method and scanner class usage with mulitple input check

--- logics executions/Class notes execution/Strings/Palindrome.java
+++ logics executions/Class notes execution/Strings/Palindrome.java
@@ -1,16 +1,20 @@
+import java.util.*;
 class Palindrome
 {
-public static void main(String[] args){
-	String str = "Madam";
+void checkPalindrom(String str){
 	String rev = "";
-for(int i = str.length()-1; i>=0; i--){
-	rev = rev + str.charAt(i);
+	for(int i= str.length()-1; i>=0; i--){
+		rev = rev + str.charAt(i);
+	}
+System.out.println(str.equals(rev)?" "+str+ " is Palindrome ":" "+str+" is not a palindrom");
 }
-
-if(rev.equalsIgnoreCase(str)){
-	System.out.println("Palindrome");
-}else{
-	System.out.println("Not a palindrome");
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+	Palindrome pd = new Palindrome();
+while(true){
+System.out.println("Enter the string check its palindrom : ");
+String str = sc.nextLine();
+pd.checkPalindrom(str);
 }
 }
 }
