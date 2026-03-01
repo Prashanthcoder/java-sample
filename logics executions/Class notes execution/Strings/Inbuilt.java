d52b3056559c4b5879fe9640717a5f1a32826ef5
Prashanthcoder <tp120113@gmail.com>
2026-03-01 23:06:12 +0530

    added the code for replace all and length string manipulation

--- logics executions/Class notes execution/Strings/Inbuilt.java
+++ logics executions/Class notes execution/Strings/Inbuilt.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Inbuilt
 {
 static void replaceallOvals(String str){
-	System.out.println(str.replaceAll("[aeiou, AEIOU]", ""));
+	System.out.println(str.replaceAll("[aeiou , AEIOU ]", ""));
 }
 static void replaceallConsonants(String str){
 	System.out.println(str.replaceAll("[^aeiou, AEIOU]", ""));
@@ -11,11 +11,35 @@ static void replaceallConsonants(String str){
 static void replaceNumbers(String str){
 	System.out.println(str.replaceAll("[0-9]", ""));
 }
+static void replacenCount(String str){
+	System.out.println(str.replaceAll(" ", "").length());
+}
+static void replaceallConsonantsCount(String str){
+	System.out.println(str.replaceAll("[^aeiou ,AEIOU ]", "").length());
+}
+static void replaceCharacter(String str){
+	System.out.println(str.replaceAll("[a-z,A-Z]", ""));
+}
+static void replaceUppernNum(String str){
+	System.out.println(str.replaceAll("[0-9,A-Z]", ""));
+}
+static void replaceAllExceptSpecials(String str){
+	System.out.println(str.replaceAll("[0-9,a-z,A-Z]", ""));
+}
+static void methodsimp(String str){
+	System.out.println(str.replace("Son", "sun"));
+}
 public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 String str = sc.nextLine();
 replaceallOvals(str);
 replaceallConsonants(str);
 replaceNumbers(str);
+replacenCount(str);
+replaceallConsonantsCount(str);
+replaceCharacter(str);
+replaceUppernNum(str);
+methodsimp(str);
+replaceAllExceptSpecials(str);
 }
 }
