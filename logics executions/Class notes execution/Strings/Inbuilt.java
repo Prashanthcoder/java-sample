d36a414f0ddd70cf562642cbf9e6d63a6f280289
Prashanthcoder <tp120113@gmail.com>
2026-03-01 22:49:05 +0530

    added the code for the number replace methods

--- logics executions/Class notes execution/Strings/Inbuilt.java
+++ logics executions/Class notes execution/Strings/Inbuilt.java
@@ -5,9 +5,17 @@ class Inbuilt
 static void replaceallOvals(String str){
 	System.out.println(str.replaceAll("[aeiou, AEIOU]", ""));
 }
+static void replaceallConsonants(String str){
+	System.out.println(str.replaceAll("[^aeiou, AEIOU]", ""));
+}
+static void replaceNumbers(String str){
+	System.out.println(str.replaceAll("[0-9]", ""));
+}
 public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 String str = sc.nextLine();
 replaceallOvals(str);
+replaceallConsonants(str);
+replaceNumbers(str);
 }
 }
