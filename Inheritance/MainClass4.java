d80627b24bc800372bc7ce57a35c5c898f13110f
Prashanthcoder <tp120113@gmail.com>
2026-03-02 15:21:46 +0530

    added the code to demonstrate hybrid inheritance

--- Inheritance/MainClass4.java
+++ Inheritance/MainClass4.java
@@ -1,18 +1,29 @@
 class Sample5
 {
+int x = 90;
 }
 class Demo5 extends Sample5
 {
+int y = 97;
 }
 class Tester5 extends Demo5
 {
+void display(){
+	System.out.println("It is from tester 5 class ");
+}
 }
 class Cool5 extends Sample5
 {
+int z = 20;
 }
 class MainClass4
 {
 public static void main(String[] args){
-	
+	Tester5 t1 = new Tester5();
+	Cool5 c1 = new Cool5();
+	System.out.println("**************this hybrid inheritance***********");
+System.out.println("from mulitlevel in hybrid "+t1.x+" "+t1.y);
+t1.display();
+System.out.println("from hierarchical inheritance "+c1.x+" "+c1.z);
 }
 }
