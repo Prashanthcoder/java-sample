ab649f4c0b60ac93844bc9cf769328aa5f36c4ae
Prashanthcoder <tp120113@gmail.com>
2026-03-02 14:24:52 +0530

    added the code to demonstrate hybrid inheritance

---
+++ Inheritance/MainClass4.java
@@ -0,0 +1,18 @@
+class Sample5
+{
+}
+class Demo5 extends Sample5
+{
+}
+class Tester5 extends Demo5
+{
+}
+class Cool5 extends Sample5
+{
+}
+class MainClass4
+{
+public static void main(String[] args){
+	
+}
+}
