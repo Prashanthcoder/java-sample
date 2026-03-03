c5285ebddba8df06abd5f0453ecda24fcec166d3
Prashanthcoder <tp120113@gmail.com>
2026-03-03 22:32:23 +0530

    added the code to demonstrate the overloading using static methods

---
+++ Method overloading/Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+static void StudentDetails(String name){
+	System.out.println("The name of the student "+name);
+}
+static void StudentDetails(String name, int id){
+	System.out.println("The id of the student : "+id+" Name : "+name);
+}
+static void StudentDetails(int result, String name, int id){
+	System.out.println("The id : "+id+" Name : "+name+" Result : "+result);
+}
+public static void main(String[] args){
+	StudentDetails("Prashanth");
+StudentDetails("Prashanth", 28);
+StudentDetails(99, "Prashanth", 28);
+	}
+}
