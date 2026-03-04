ecbe17113eb997fbd885cbc62554aac46f0ced8f
Prashanthcoder <tp120113@gmail.com>
2026-03-04 22:48:07 +0530

    practiced the java swap

---
+++ practice/Practice1.java
@@ -0,0 +1,21 @@
+class Practice1
+{
+void SwapWithTemp(int a, int b){
+	int temp = a;
+a = b;
+b = temp;
+
+System.out.println("the values after swapped : a = "+ a+ " b = "+b);
+}
+void SwapWithoutTemp(int a, int b){
+	a = a+b;
+	b = a-b;
+	a = a-b;
+System.out.println("the values after swapped : a = "+ a+ " b = "+b);
+}
+public static void main(String[] args){
+	Practice1 p = new Practice1();
+p.SwapWithTemp(34, 98);
+p.SwapWithoutTemp(344, 9834);
+}
+}
