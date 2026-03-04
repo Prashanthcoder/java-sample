18de2697abf0157d7d63548b334231e060149441
Prashanthcoder <tp120113@gmail.com>
2026-03-04 23:32:09 +0530

    added the code to demonstrate the code for interchange

---
+++ practice/Practice4.java
@@ -0,0 +1,19 @@
+class Practice4
+{
+static String rev(String str){
+	String res = "";
+for(int i = str.length()-1; i>=0; i--){
+	res = res + str.charAt(i);
+}
+return res;
+}
+
+public static void main(String[] args){
+	String str = "hi hello macha yen madtidia";
+System.out.println(str);
+String[] s = str.split(" ");
+for(int i = s.length-1; i>=0;i--){
+	System.out.print(s[i]+" ");
+}
+}
+}
