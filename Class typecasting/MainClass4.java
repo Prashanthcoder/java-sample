4c5b11dde4dc6c1a85e5b2dea349fb0c0a8f86ec
Prashanthcoder <tp120113@gmail.com>
2026-03-06 22:15:55 +0530

    added the code for upcasting n downcasting

---
+++ Class typecasting/MainClass4.java
@@ -0,0 +1,25 @@
+class Demo1
+{
+double x = 34;
+}
+class Dumtaka extends Demo1
+{
+int x = 23;
+void disp(){
+	System.out.println("parent x : "+super.x);
+}
+}
+
+class MainClass4 
+{
+	public static void main(String[] args) 
+	{
+Demo1 d1 = new Dumtaka();
+System.out.println("****upcasting****");
+System.out.println(d1.x);
+Dumtaka du = (Dumtaka)d1;
+System.out.println("****downcasting***");
+du.disp();
+System.out.println(du.x);
+	}
+}
