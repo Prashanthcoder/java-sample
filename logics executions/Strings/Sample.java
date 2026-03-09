04914ef6c461063500b0e0c55dd1bf849490b48d
Prashanthcoder <tp120113@gmail.com>
2026-03-09 23:59:54 +0530

    added the code sample

---
+++ logics executions/Strings/Sample.java
@@ -0,0 +1,13 @@
+class Sample
+{
+public static void main(String[] args){
+	String str = "hi how are r uuu u u u ";
+String[] s = str.split(" ");
+for(int i = 0;i<s.length-1;i++){
+	if(!s[i].equals(s[i+1]){
+		System.out.print(s[i]);
+	}
+}
+System.out.print(s[i]);
+}
+}
