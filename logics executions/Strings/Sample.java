a8980fad624003652d0277e767c9427e7102dc91
Prashanthcoder <tp120113@gmail.com>
2026-03-10 00:03:34 +0530

    fixed the exception in the array

--- logics executions/Strings/Sample.java
+++ logics executions/Strings/Sample.java
@@ -1,13 +1,14 @@
 class Sample
 {
 public static void main(String[] args){
-	String str = "hi how are r uuu u u u ";
+	String str = "hi hi hi how how how are are r uuu u u u ";
 String[] s = str.split(" ");
 for(int i = 0;i<s.length-1;i++){
-	if(!s[i].equals(s[i+1]){
-		System.out.print(s[i]);
+	if(!s[i].equals(s[i+1]))
+{
+		System.out.print(s[i]+" ");
 	}
 }
-System.out.print(s[i]);
+System.out.print(s[s.length-1]);
 }
 }
