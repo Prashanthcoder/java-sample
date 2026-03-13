07f7a0a7199cd06b939dabafc4ad8ff37a311649
Prashanthcoder <tp120113@gmail.com>
2026-03-13 15:20:46 +0530

    simple code

---
+++ Meow.java
@@ -0,0 +1,9 @@
+public class Meow{
+public static void main(String[] args){
+System.out.println("Hello world!");
+}}
+public class Hello(){
+void disp(){
+System.out.println("What are you doing");
+}
+}
