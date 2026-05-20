15ba7ac37a1317415cd65d9b0e96bdaba658664e
Prashanthcoder <tp120113@gmail.com>
2026-05-20 23:43:50 +0530

    ByPassing the SSL certification, updated the bypass

--- jsoup_scraper/JobScraper.java
+++ jsoup_scraper/JobScraper.java
@@ -1,5 +1,6 @@
 package com.prashanth.jsoup_scraper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,19 +8,29 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 
 public class JobScraper {
 	private static final String BASE_URL =  "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=java&txtLocation=";
 	
-	public List<Job> scrape() throws Exception{
+	public List<Job> scrape() {
 		List<Job> jobs = new ArrayList<Job>();
-		
+		try {
+			disableSSLVerification();
 		System.out.println("Connecting to TimeJobs...");
 		
 		Document doc = Jsoup.connect(BASE_URL)
-				.userAgent("Mozilla/5.0")
-				.timeout(15000)
-				.get();
+				  .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120.0.0.0 Safari/537.36")
+	                .header("Accept-Language", "en-US,en;q=0.9")
+	                .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+	                .header("Connection", "keep-alive")
+	                .ignoreHttpErrors(true)
+	                .followRedirects(true)
+	                .timeout(30000)
+	                .get();
 		
 		Elements jobCards = doc.select("li.clearfix.job-bx.what-shd-bx");
 		
@@ -42,6 +53,26 @@ public class JobScraper {
 			jobs.add(new Job(title, company, url, salary, location));
 			
 		}
+		 } catch (IOException e) {
+	            System.err.println(" Error scraping: " + e.getMessage());
+	        }
 		return jobs;
+	}
+	
+	private void disableSSLVerification() {
+	    try {
+	        TrustManager[] trustAll = new TrustManager[]{
+	            new X509TrustManager() {
+	                public X509Certificate[] getAcceptedIssuers() { return null; }
+	                public void checkClientTrusted(X509Certificate[] c, String a) {}
+	                public void checkServerTrusted(X509Certificate[] c, String a) {}
+	            }
+	        };
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAll, new java.security.SecureRandom());
+	        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	    } catch (Exception e) {
+	        System.err.println("SSL setup failed: " + e.getMessage());
+	    }
 	}
 }
