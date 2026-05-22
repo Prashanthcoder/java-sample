6f976e997f6aaf07397eabc3a9630df8d2f3d61d
Prashanthcoder <tp120113@gmail.com>
2026-05-22 19:29:05 +0530

    flex entities

---
+++ task1/Flex.java
@@ -0,0 +1,37 @@
+package com.prashanth.jsoup_scraper;
+
+public class Flex {
+		private int id;
+		private String name;
+		private String job_description;
+		private String companyName;
+		private String salary;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id= id;		}
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getJob_description() {
+			return job_description;
+		}
+		public void setJob_description(String job_description) {
+			this.job_description = job_description;
+		}
+		public String getCompanyName() {
+			return companyName;
+		}
+		public String getSalary() {
+			return salary;
+		}
+		public void setSalary(String salary) {
+			this.salary   = salary;
+		}
+}
