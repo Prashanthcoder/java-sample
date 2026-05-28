ef8d5609cd0db769211070b02a7948b329310a5a
Prashanthcoder <tp120113@gmail.com>
2026-05-28 22:27:02 +0530

    prime nunbers v2

--- classtask_2805/SumOfPrimeRange.java
+++ classtask_2805/SumOfPrimeRange.java
@@ -1,10 +1,14 @@
 package classtask_2805;
-
+import java.util.Scanner;
 public class SumOfPrimeRange {
 	public static void main(String[] args) {
-		int range = 50;
-		int sum =0;
-		for(int i = 2;i<=range;i++) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("number of prime to add ");
+		int n = sc.nextInt();
+		int sum =0, count = 0;
+		int i = 2;
+		while(count<n) {
+//		for(int i = 2;i<=1000;i++) {
 			boolean f = true;
 			for(int j = 2;j*j<=i;j++) {
 				if(i%j==0) {
@@ -15,8 +19,11 @@ public class SumOfPrimeRange {
 			if(f) {
 				System.out.print(i+" ");
 				sum= sum+i;
+				count++;
 			}
+			i++;
 		}
-		System.out.println(sum);
+//		}
+		System.out.println("\nthe sum of these above numbers are: "+sum);
 	}
 }
